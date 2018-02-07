a924daf389f56163b8f48093ff28397b1799654b
Eduardo DuQuesne <eduquesne@eduardos-mbp.attlocal.net>
2018-02-06 23:18:21 -0600

    Store Arp Synth Sound

--- javascripts/set-settings.js
+++ javascripts/set-settings.js
@@ -7,10 +7,9 @@ const Tone = require('tone');
 const firebase = require("./config/fbConfig");
 const loops = require('./loops');
 
-
-
 module.exports.storeSettings = (input) => {
     let currentChords = loops.getChords();
+    let arpSoundName = loops.storeArpSound();
     let settings = {
     //uid//
     uid: firebase.auth().currentUser.uid,
@@ -24,6 +23,8 @@ module.exports.storeSettings = (input) => {
     arpPanDisplay: interface.arpVolPanKnob.x,
     arpVol: fx.arpVolPan.volume.input.value,
     arpVolDisplay: interface.arpVolPanKnob.y,
+    //arp sound//
+    arpSound: arpSoundName,
     //bass//
     bassMatrix: interface.bassSequencer.matrix.pattern,
     bassPan: fx.bassVolPan.pan.value,
@@ -36,13 +37,20 @@ module.exports.storeSettings = (input) => {
     beatPanDisplay: interface.beatVolPanKnob.x,
     beatVol: fx.beatVolPan.volume.input.value,
     beatVolDisplay: interface.beatVolPanKnob.y,  
-    //delay//
+    //arp delay//
     delayOneWet: fx.delayOne.wet.value, 
     delayOneDisplay: interface.dialDelayWet.value,
     delayOneFB: fx.delayOne.feedback.value,
     delayOneFBDisplay: interface.dialDelayFeedback.value,
     delayOneTime: fx.delayOne.delayTime.value,
     delayOneTimeDisplay: interface.dialDelayTime.value,
+    //arp reverb//
+    reverbOneWet: fx.reverbOne.wet.value,
+    reverbOneWetDisplay: interface.dialReverbWet.value,   
+    reverbOneDampening: fx.reverbOne.dampening.value,
+    reverbOneDampeningDisplay: interface.dialReverbDampening.value,
+    // reverbOneRoomSize: fx.reverbOne.roomSize.value,
+    // reverbOneRoomSizeDisplay: interface.dialReverbRoomSize.value,
     //Chords//
     chords: currentChords
     };
@@ -62,6 +70,8 @@ module.exports.recallSetting = (setting) => {
     interface.arpVolPanKnob.x = setting.arpPanDisplay;
     fx.arpVolPan.volume.input.value = setting.arpVol;
     interface.arpVolPanKnob.y = setting.arpVolDisplay;
+    //arp sound//
+    loops.recallArpSound(setting.arpSound);
     //bass//
     interface.bassSequencer.matrix.set.all(setting.bassMatrix);
     fx.bassVolPan.pan.value = setting.bassPan;
@@ -74,12 +84,18 @@ module.exports.recallSetting = (setting) => {
     interface.beatVolPanKnob.x = setting.beatPanDisplay;
     fx.beatVolPan.volume.input.value = setting.beatVol;
     interface.beatVolPanKnob.y = setting.beatVolDisplay;
-    //delay//
+    //arp delay//
     fx.delayOne.wet.value = setting.delayOneWet;
     interface.dialDelayWet.value = setting.delayOneDisplay;
     fx.delayOne.feedback.value = setting.delayOneFB;
     interface.dialDelayFeedback.value = setting.delayOneFBDisplay;
     fx.delayOne.delayTime.value = setting.delayOneTime;
     interface.dialDelayTime.value = setting.delayOneTimeDisplay;
-    
+    //arp reverb//
+    fx.reverbOne.wet.value = setting.reverbOneWet;
+    interface.dialReverbWet.value = setting.reverbOneWetDisplay;    
+    fx.reverbOne.dampening.value = setting.reverbOneDampening;
+    interface.dialReverbDampening.value = setting.reverbOneDampeningDisplay;
+    // fx.reverbOne.roomSize.value = setting.reverbOneRoomSize;
+    // interface.dialReverbRoomSize.value = setting.reverbOneRoomSizeDisplay;
 };
