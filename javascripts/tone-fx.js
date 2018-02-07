585d9c220ef399d19c1c57944c9377ea106e142a
Eduardo DuQuesne <eduquesne@Eduardos-MacBook-Pro.local>
2018-02-06 19:41:10 -0600

    Add Instruments

--- javascripts/tone-fx.js
+++ javascripts/tone-fx.js
@@ -7,9 +7,13 @@ const Tone = require('tone');
 module.exports.delayOne = new Tone.FeedbackDelay();
 // delayOne.wet.value = 0;
 
-module.exports.phaserOne = new Tone.Phaser();
+module.exports.reverbOne = new Tone.Freeverb();
 // phaserOne.wet.value = 0;
 
 module.exports.arpVolPan = new Tone.PanVol();
 module.exports.bassVolPan = new Tone.PanVol();
 module.exports.beatVolPan = new Tone.PanVol();
+
+//Compressor
+module.exports.masterComp = new Tone.Compressor(-30, 3);
+
