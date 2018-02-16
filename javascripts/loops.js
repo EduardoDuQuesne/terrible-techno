09496fd20ca6fc365ba740fb2b1d18de425dfc23
Eduardo DuQuesne <eduquesne@eduardos-mbp.attlocal.net>
2018-02-15 23:35:28 -0600

    Tempo Gradient Remove Classes

--- javascripts/loops.js
+++ javascripts/loops.js
@@ -199,7 +199,7 @@ let bassLoop = new Tone.Sequence((time, col) => {
 //Gradient Loop
 let lastNumber = 0;
 let styleLoop = new Tone.Sequence((time, col) => {
-    $('body').removeClass(`tempo-gradient-${lastNumber}`);
+    $('body').removeClass();
     $('body').addClass(`tempo-gradient-${col}`);
     lastNumber = col;
 }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31], "32n");
