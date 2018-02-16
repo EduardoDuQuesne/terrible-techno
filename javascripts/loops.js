eebc04a3cab508c5456f14adafb13ad9e7713b67
Eduardo DuQuesne <eduquesne@eduardos-mbp.attlocal.net>
2018-02-15 23:30:25 -0600

    Tempo Gradient

--- javascripts/loops.js
+++ javascripts/loops.js
@@ -196,6 +196,14 @@ let bassLoop = new Tone.Sequence((time, col) => {
     }
 }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n");
 
+//Gradient Loop
+let lastNumber = 0;
+let styleLoop = new Tone.Sequence((time, col) => {
+    $('body').removeClass(`tempo-gradient-${lastNumber}`);
+    $('body').addClass(`tempo-gradient-${col}`);
+    lastNumber = col;
+}, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31], "32n");
+
 
 ///// DRUM LOOP /////
 //Sounds//
@@ -232,10 +240,7 @@ let chordLoop = new Tone.Sequence((time, col) => {
     seqKey = noteNames[arpKeys[col]];
     seqKeyBass = noteNamesBass[arpKeys[col]];
     $(`.chord-${col}`).addClass('border');
-    $(`.chord-${col}`).siblings().removeClass('border');
-    // $('.body-colors').css("background-color", `${colors[col]}`);
-    // console.log('Check', colors[col] );
-    
+    $(`.chord-${col}`).siblings().removeClass('border');    
 }, [0, 1, 2, 3, 4, 5, 6, 7], "1n");
 
 module.exports = {
@@ -245,6 +250,7 @@ module.exports = {
     drums,
     drumLoop,
     chordLoop,
+    styleLoop,
     changeChords,
     loadChords,
     getChords,
